cd13d09bedb33c84b083692ec438267b8197ad14
Stephen Nagasawa <stephen.nagasawa@me.com>
2022-05-27 14:54:18 -0600

    Updated Dice roller

--- flask_app/static/roller_script.js
+++ flask_app/static/roller_script.js
@@ -57,8 +57,7 @@ function rollDice(){
 }
 
 function selectDie(val){
-    die = val;
-    curDie.innerText = 'You have selected D' + die;
+    curDie.innerText = 'You have selected D' + val;
 }
 
 function selectNum(num){
@@ -70,4 +69,25 @@ function selectNum(num){
 function selectRoll(rollType){
     type = rollType;
     curType.innerText = 'You have selected ' + type + ' roll';
+}
+let dieOption = [4,6,8,10,12,20]
+
+function makeDieBig(id){
+    for(var i = 0; i<dieOption.length; i ++){
+        if(dieOption[i] !== id){
+            document.getElementById(dieOption[i]).style.height='90px';
+            document.getElementById(dieOption[i]).style.width='90px';
+        }
+        document.getElementById(id).style.height='121px';
+        document.getElementById(id).style.width='121px';
+    }
+}
+
+
+
+function resetDice(){
+    for(i = 0; i <dieOption.length; i++){
+        document.getElementById(dieOption[i]).style.height='90px';
+        document.getElementById(dieOption[i]).style.width='90px';
+    }
 }
